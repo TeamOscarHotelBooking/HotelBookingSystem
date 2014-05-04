7e28f1e11b26219e21562d96d9eb7bf74801dd48
een828 <NaifAlmakhdhub2013@u.northwestern.edu>
2014-05-03 21:09:31 -0500

    Create Room.java

---
+++ Room.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hbs;
+
+/**
+ *
+ * @author Team Oscar
+ */
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Room {
+    
+    protected int number;
+    protected RoomType type ;
+    protected double price;
+    protected Calendar Date;
+    protected RoomState state;
+    
+    /**
+     * Default Constructor
+     * Numbers are random currently to ease the process for other methods in the project
+     */
+    public Room(){
+    this.number = 1;
+    this.type = RoomType.SINGLE;
+    this.price = 100;
+    this.Date =  new GregorianCalendar();
+    this.state = RoomState.AVAILABLE;
+        
+    }
+    
+    public Room (int number,RoomType type,double price){
+    this.number = number;
+    this.type = type;
+    this.price = price;
+    this.Date =  new GregorianCalendar();
+    this.state = RoomState.AVAILABLE;
+    }
+    
+    /**
+     * Room Type Transformer
+     * @param type 
+     */
+    public void setType(RoomType type){
+        this.type = type;
+    }
+    
+    /** 
+     * Room Price Transformer
+     * @param price 
+     */
+    public void setPrice(double price){
+        this.price = price;
+    }
+    
+    /**
+     * Room State Transformer
+     * @param state 
+     */
+    public void setState(RoomState state){
+        this.state = state;
+    }
+    
+    /**
+     * Room Type observer
+     * @return type
+     */
+    public RoomType getType(){
+        return this.type;
+    }
+    
+    /**
+     * Room Price Observer
+     * @return price
+     */
+    public double getPrice(){
+       return this.price;
+    }
+    
+    /**
+     * Room State observer
+     * @return state
+     */
+    public RoomState getState(){
+        return this.state;
+    }
+    
+    /**
+     * Function to make a discount on a room (in case of a special offer)
+     * Will change the original price to new discounted one
+     * @param DiscountPercentage 
+     */
+    public void DiscountPrice(double DiscountPercentage){
+        if (DiscountPercentage <1 && DiscountPercentage > 0){
+        this.price = (1-DiscountPercentage)*this.price;
+        }
+        else {
+            System.out.println("Non authorized Discount");
+        }
+    }
+    
+    /**
+     * Function to check if the room is booked or not 
+     * @return true if the room is available
+     */
+    public boolean isEmpty(){
+        if (this.state == RoomState.BOOKED){
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+}
