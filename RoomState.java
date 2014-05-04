84df34ab3790ea8a33f32740fff8d44d5f20510e
een828 <NaifAlmakhdhub2013@u.northwestern.edu>
2014-05-03 21:11:22 -0500

    Create RoomState.java

---
+++ RoomState.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hbs;
+
+/**
+ *
+ * @author Team Oscar
+ */
+public enum  RoomState {
+    BOOKED, AVAILABLE
+}
