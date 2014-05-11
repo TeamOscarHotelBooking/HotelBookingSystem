beb527d769f568143df4506f6d4492b67a8780e3
GregoryElliott <gregoryelliott34@yahoo.com>
2014-05-11 15:27:58 -0500

    new

--- Reservation.java
+++ Reservation.java
@@ -4,32 +4,41 @@
  * and open the template in the editor.
  */
 
-package hotel;
+package hbs;
 
 /**
  *
  * @author gregoryelliott34
  */
 public class Reservation {
-    private Hotel [] arrayOfHotels;
-    private numHotels;
+    private String user;
+    private Hotel chosenHotel;
+    private int idNumber;
+    private Room chosenRoom;
     
-    public Reservation(int numberOfHotels) {
-        numHotels = numberOfHotels;
-        for(int i = 0; i<numHotels; i++) {
-            arrayOfHotels[i] = new Hotel();
-        }
+    public Reservation() {
     }
     
-    public Hotel[] Search(String location) {
-        Hotel[] hotelsInLocation = new Hotel[numHotels];
+    public void Search(String desiredLocation, String hotelName) {
+        Location targetLocation;
+        Hotel targetHotel;
+        Room freeRoom;
+        for(int i=0; i<arrayOfLocations.length; i++) {
+            if((arrayOfLocations[i]).getCity() == desiredLocation) {
+                targetLocation = arrayOfLocations[i];
+            }
+        }
+        /*Hotel[] hotelsInLocation = new Hotel[numHotels];
         int numHotelsInLocations = 0;
         for (int i = 0; i<arrayOfHotels.length; i++) {
             if((arrayOfHotels[i].getLocation()).equals(location)) {
                 hotelsInLocation[numHotelsInLocation] = arrayOfHotels[i];
             }
         }
-        return hotelsInLocation;
+        return hotelsInLocation;*/
+        targetHotel = targetLocation.SearchForHotel(hotelName);
+        freeRoom = targetHotel.getFreeRoom();
+        freeRoom.setState("booked");
     }
     
 }
