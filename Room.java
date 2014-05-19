6c1f810897f1897724583e51e31bafd46d345f84
een828 <NaifAlmakhdhub2013@u.northwestern.edu>
2014-05-19 12:40:50 -0500

    Update Room.java

--- Room.java
+++ Room.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-public class Room implements Serializable;{
+public class Room implements Serializable{
     
     protected int number;
     protected RoomType type;
