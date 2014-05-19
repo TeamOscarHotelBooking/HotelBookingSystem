71a3b2fb982bb4cbfc3a24b854f9ae950cc3bbf6
zzynwu <zzynwu@users.noreply.github.com>
2014-05-19 18:24:30 -0500

    Update Reservation.java

--- Reservation.java
+++ Reservation.java
@@ -5,7 +5,7 @@
  * and open the template in the editor.
  */
 
-package hbs;
+package HBS;
 import java.util.Scanner;
 //import java.io.Serializable;
 //import java.lang.Object;
@@ -19,7 +19,7 @@ public class Reservation {
     private String user;
     private Hotel chosenHotel;
     private int idNumber;
-    private Room[] chosenRoom;
+    private ArrayList<Room> chosenRoom;
     private Location location;
     private ArrayList<Location> arrayOfLocations;  // Why put it in the reservation class. It can be a public database.
     private Boolean isCancelled;    // 
@@ -33,10 +33,10 @@ public class Reservation {
         idNumber = 0;
         TotalCost=0;
         isCancelled = false;
+        chosenHotel = null;
+        chosenRoom = new ArrayList<Room>();
         
     }
-    
-    
     
     public void Search(String desiredLocation, DatePair date) {
         for(int i=0; i<arrayOfLocations.size(); i++) {
@@ -44,83 +44,87 @@ public class Reservation {
                 location = arrayOfLocations.get(i);
             }
         }
-        ArrayList<Hotel> hotelsInLocation = location.getCityHotelDataBase();
-        String hotelOptions = "The following hotels are located in " + location.getCity() + ": " + hotelsInLocation.get(0).getHotelName();
-        for (int i = 1; i<hotelsInLocation.size(); i++) {
-            hotelOptions = hotelOptions + ", " + (hotelsInLocation.get(i)).getHotelName() ;
+        System.out.println( "\t\t"+location.getCity());
+        System.out.print("Choice Number\tHotel Name\t Number of Single Rooms \t Single Room Price");
+        System.out.print("\tNumber of Double Rooms \t Double Room Price");
+        System.out.print("\t Number of JS Rooms \t JS Room Price\n");
+        for(int i=0; i<location.getCityHotelDataBase().size();i++){
+            location.getCityHotelDataBase().get(i).toString();
+            ArrayList<Room> SingleRooms = location.getCityHotelDataBase().get(i).getFreeRooms(date, RoomType.SINGLE);
+            ArrayList<Room> DoubleRooms = location.getCityHotelDataBase().get(i).getFreeRooms(date, RoomType.DOUBLE);
+            ArrayList<Room> JSRooms = location.getCityHotelDataBase().get(i).getFreeRooms(date, RoomType.JUNIORSUITE);
+            System.out.print((i+1)+"\t\t"+location.getCityHotelDataBase().get(i).getHotelName()+ "\t");
+            System.out.print(SingleRooms.size()+ "\t\t"+ SingleRooms.get(0).getPrice()+"\t\t");
+            System.out.print(DoubleRooms.size()+ "\t\t"+ DoubleRooms.get(0).getPrice()+"\t\t");
+            System.out.print(JSRooms.size()+ "\t\t"+ JSRooms.get(0).getPrice()+"\n");
         }
-        System.out.println(hotelOptions);
-	
-	
-	
-	
-	
-	
-	
-	/*
-        System.out.println("Which Hotel do you want to reserve?");
-        Scanner keyboard = new Scanner(System.in);
-        String hotelName;
-        hotelName = keyboard.nextLine();
-        for (int i = 0; i<hotelsInLocation.size(); i++) {
-            if(((hotelsInLocation.get(i)).getHotelName()).equals(hotelName))
-                chosenHotel = hotelsInLocation.get(i); 
-        }
-	
+        Scanner UserInput= new Scanner(System.in);
+        System.out.print("Enter the NUMBER of hotel you choose: ");
+        boolean keepgoing=true;
+        while(keepgoing){
+            if(UserInput.hasNextInt()){
+                chosenHotel=location.getCityHotelDataBase().get(UserInput.nextInt());
+                keepgoing=false;
+            }
+            else{
+                System.out.println("Error");
+                keepgoing=true;
+            }
+        }        
+    }
     
+    public void chooserooms(DatePair date){
+        chosenDate = date;
+        chosenHotel.getHotelName();
+        ArrayList<Room> SingleRooms = chosenHotel.getFreeRooms(date, RoomType.SINGLE);
+        ArrayList<Room> DoubleRooms = chosenHotel.getFreeRooms(date, RoomType.DOUBLE);
+        ArrayList<Room> JSRooms = chosenHotel.getFreeRooms(date, RoomType.JUNIORSUITE);
+	Scanner UserInput= new Scanner(System.in);
+        System.out.print("Enter the TYPE of rooms you want to book, 1 for SINGLE, 2 for DOUBLE and 3 for JS: ");
+        int type=UserInput.nextInt();
+        System.out.print("Enter the NUMBER of rooms you want to book: ");
+        int num=UserInput.nextInt();
         
-	
-	chosenDate = date;
-	chosenHotel.getHotelName();
-	ArrayList<Room> SingleRooms = chosenHotel.getFreeRooms(date, RoomType.SINGLE);
-	ArrayList<Room> DoubleRooms = chosenHotel.getFreeRooms(date, RoomType.DOUBLE);
-	ArrayList<Room> JSRooms = chosenHotel.getFreeRooms(date, RoomType.JUNIORSUITE);
-	*/
-	
-        //Room[] rooms = chosenHotel.getFreeRooms(date);
-		/*
-        String roomOptions = "The following rooms are available : " + rooms[0].toString();
-        for (int i = 1; i<rooms.length; i++) {
-            roomOptions = roomOptions + ", " + rooms[i].toString() ;
-        }
-        System.out.println(roomOptions);
-        System.out.println("Enter the room numbers for each room you want to reserve seperated by commas:");
-        Scanner keyboard = new Scanner(System.in);
-        int j=0;
-        while (keyboard.hasNextInt()){
-            int roomNumber;
-            roomNumber = keyboard.nextInt();
-            for (int i = 1; i<rooms.length; i++) {
-                if(rooms[i].getNumber()==roomNumber){
-                    chosenRoom[j] = rooms[i];
-                    j++;
-                }
+        switch(type){
+            case 1: {
+                for(int i=0; i<num; i++) 
+                    chosenRoom.add(SingleRooms.get(i));
+                break;
+            }
+            case 2:{
+                for(int i=0; i<num; i++) 
+                    chosenRoom.add(DoubleRooms.get(i));
+                break;
+            }
+            case 3:{
+                for(int i=0; i<num; i++) 
+                    chosenRoom.add(JSRooms.get(i)); 
+                break;
             }
         }
-        for(int i=0; i<j; i++)
-        chosenRoom[i].insert(chosenDate);
-				*/
+        for(int i=0; i<chosenRoom.size();i++)
+            chosenRoom.get(i).insert(date);
     } 
     
-    public void CalculateCost(Room[] rooms){
-        for(int i=0; i<rooms.length; i++){
-            TotalCost=TotalCost+rooms[i].getPrice();
+    public void CalculateCost(ArrayList<Room> rooms){
+        for(int i=0; i<rooms.size(); i++){
+            TotalCost=TotalCost+rooms.get(i).getPrice();
         }
     }
     
     public void confirmReservation(String name, int ID, String desiredLocation, DatePair date){
         user=name;
         idNumber=ID;
-        this.SearchForLocation(desiredLocation);
-        this.SearchForDate(date);
+        this.Search(desiredLocation, date);
+       // this.SearchForDate(date);
         this.CalculateCost(chosenRoom);
         // Add the confiremed reservation to the arraylist of reservation generated in Main function
     }
     
     public void cancelReservation() {
         isCancelled = false;
-        for(int i=0; i<chosenRoom.length; i++)
-            chosenRoom[i].cancel(this.chosenDate);
+        for(int i=0; i<chosenRoom.size(); i++)
+            chosenRoom.get(i).cancel(this.chosenDate);
     }
     
     public String getUser() {
@@ -135,7 +139,7 @@ public class Reservation {
         return idNumber;
     }
     
-    public Room[] getChosenRoom() {
+    public ArrayList<Room> getChosenRoom() {
         return chosenRoom;
     }
     
@@ -145,5 +149,9 @@ public class Reservation {
     
     public Boolean getIsCancelled() {
         return isCancelled;
+    }
+    @Override
+    public String toString(){
+        return this.idNumber+"  "+this.user+"  "+this.chosenHotel.getHotelName()+"  "+this.chosenDate;
     }
 }
