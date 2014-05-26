d78846c1dc60f2566060c1db0d7de193c7eb5248
zzynwu <zzynwu@users.noreply.github.com>
2014-05-26 15:41:14 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package hbs;
+package HBS;
 import java.util.ArrayList;
 /**
  *
@@ -33,11 +33,13 @@ public class Customer implements User {
     public Customer(){
         this.name=null;
         this.password="0123456789";
+        reservation= new ArrayList<>();
     }
     // constructor a customer using name
     public Customer(String name){
         this.name=name;
         this.password="0123456789";
+        reservation= new ArrayList<>();
     }
     public void setpassword(String password){
         this.password=password;
@@ -55,9 +57,6 @@ public class Customer implements User {
     public void reserve(ArrayList<Location> aol,int id,String desiredlocation, DatePair date ){
         Reservation res=new Reservation(aol);
         res.confirmReservation(this.name, id, desiredlocation, date);
-        /* setup the reservation based on the customer's choice.
-        need more details in reservation class.
-        */
         reservation.add(res); // add the new reservation to the list.
     }
     public Reservation getMostRecentReservation(){
@@ -66,6 +65,7 @@ public class Customer implements User {
             return this.reservation.get(size-1);
         else return null;
     }
+    
     @Override
     // cancel a reservation  
     public void cancel(int id){
@@ -86,9 +86,12 @@ public class Customer implements User {
     public void showreservation(){
        int size=reservation.size();
        String output="All the reservation info:";
-       for (int i=0; i<size; i++){
-           output=output+reservation.get(i).toString(); // need a toString method in reservation class.
+       if(size>=1){
+            for (int i=0; i<size; i++){
+                output=output+reservation.get(i).toString(); // need a toString method in reservation class.
+            }
        }
+       else output="No reservations.";
        System.out.println(output);
     }
     
