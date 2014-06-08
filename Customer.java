f82a799ebb6e39ac0d080891851d04d17852cda3
zzynwu <zzynwu@users.noreply.github.com>
2014-06-07 19:20:13 -0500

    add java document description

--- Customer.java
+++ Customer.java
@@ -5,55 +5,64 @@
  */
 
 package hbs;
-import hbs.DatePair;
-import hbs.Location;
-import hbs.Reservation;
-import hbs.User;
+import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.ArrayList;
 /**
  *
  * @author zhangzy
  *
- */
-/*
-I have some suggestions about the reservation class based on my implements of the customer class:
-IsEqual();             compare the this reservation ID to a given ID, see if it is the reservation we want to get.
-Room[] searchForRoom(hotel, datepair);      return all the rooms which is available during datepairin this hotel.
-confirmreservation(Hotel, datepair);{
-    random generate a reservation ID;
-    set all of the reservation info;
-    push it into the reservation arraylist.
-}
-toString();     
 */
-public class Customer implements User { 
+
+public class Customer implements User, Serializable { 
    
     private String name;
     private String password;
     private ArrayList<Reservation> reservation;
     private int creditcard;
     
-    private static RuntimeException invalidReservationIDException = new RuntimeException("Invalid ID Number: Reservation ID needs to be a positive integer");
+    /**
+     * default constructor.
     
-    //default constructor
+    */
     public Customer(){
         this.name=null;
         this.password="0123456789";
     }
-    // constructor a customer using name
+    /** 
+       * constructor a customer using name
+       * @param name the name of the customer
+    */
     public Customer(String name){
         this.name=name;
         this.password="0123456789";
     }
-    public String getName() {
-        return this.name;
+    /**
+	 * used in GUI
+	 * @param name the name of the customer
+	 * @param password the password of the customer
+    */
+    public Customer(String name, String password){
+        this.name=name;
+        this.password=password;
+		this.reservation = new ArrayList<Reservation>();
     }
+    /**
+        * @param password the password of the customer
+        * Set the password of the customer.
+    */
     public void setpassword(String password){
         this.password=password;
     }
+    /**
+        * Get the password of the customer.
+    */
     public String getpassword(){
         return this.password;
     }
+    /**
+       * get the username.
+    */
     public String showname(){
         return this.name;
     }
@@ -64,11 +73,30 @@ public class Customer implements User {
     public void reserve(ArrayList<Location> aol,int id,String desiredlocation, DatePair date ){
         Reservation res=new Reservation(aol);
         res.confirmReservation(this.name, id, desiredlocation, date);
-        /* setup the reservation based on the customer's choice.
-        need more details in reservation class.
-        */
         reservation.add(res); // add the new reservation to the list.
     }
+	
+	/**
+	 * the parallel reserve method used in GUI
+	 * @param aol a list of locations
+	 * @param id the reservation ID
+	 * @param desiredlocation the name of the chosen location
+	 * @param date the date range wanted to reserve
+	 * @param hotelIndex the index of the chosen hotel
+	 * @param roomTypeIndex the index of chosen roomType
+	 * @param numRoomIndex they index of chosen number of rooms
+	 */
+	public void reserveGUI(ArrayList<Location> aol, int id, String desiredlocation, DatePair date,
+			int hotelIndex, int roomTypeIndex, int numRoomIndex){
+		
+        Reservation res = new Reservation(aol, LocalDate.now());
+        res.allInOne(this.name, id, desiredlocation, date, hotelIndex, roomTypeIndex, numRoomIndex);
+        reservation.add(res); 
+    }
+    /**
+     * Get the most recent reservation in the list.
+             
+    */  	
     public Reservation getMostRecentReservation(){
         int size=this.reservation.size();
         if(size>0)
@@ -76,14 +104,13 @@ public class Customer implements User {
         else return null;
     }
     @Override
-    // cancel a reservation  
+    /**
+     * cancel a reservation.
+     * @param id the id number of the reservation.
+             
+    */  
     public void cancel(int id){
       //  Reservation res = new Reservation();
-        if (id < 0 || id != (int) id)
-        {
-            throw invalidReservationIDException;
-        }
-        
         boolean state=true;
         for (int i=0; i<this.reservation.size();i++){
            if( reservation.get(i).getIDNumber()==id){
@@ -97,14 +124,24 @@ public class Customer implements User {
             System.out.println("Can't find the reservation!");
     }
     @Override
+    /**
+        * A toString method of the reservation list of the customer.
+    */
     public void showreservation(){
        int size=reservation.size();
-       String output="Reservation info:";
+       String output="All the reservation info:";
        for (int i=0; i<size; i++){
-           output=output+"\n" + reservation.get(i).toString(); // need a toString method in reservation class.
+           output=output+reservation.get(i).toString(); // need a toString method in reservation class.
        }
        System.out.println(output);
     }
     
-    
+	/**
+	 * used in GUI
+	 * @return a list of all reservation of this customer
+	 */
+    public ArrayList<Reservation> getReservationList() {
+		return this.reservation;
+	}
 }
+
